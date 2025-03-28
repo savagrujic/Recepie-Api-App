beab4199a84e9dce846f4a9698b22589d513e74c
savagrujic <savagrujic42@gmail.com>
2025-03-28 22:10:11 +0000

    added 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Page404 from './404.jsx'
 import Product from './Product.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <Routes>
     <Route  path='/' element= {<App />}/>
     <Route  path='/product/:id' element= {<Product />}/>
+    <Route path='*' element = {<Page404 />}/>
   </Routes>
   </BrowserRouter>
 )

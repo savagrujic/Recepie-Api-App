906cc03b32612a969d2a2d5ce6966acfd00d4f43
savagrujic <savagrujic42@gmail.com>
2025-03-28 22:15:16 +0000

    Styled 404 page

--- src/404.jsx
+++ src/404.jsx
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router-dom'
+import img from './assets/404.jpg'
 export default function Page() {
 
     return(
-        <div className="wrap">
-
+        <div>
+        <div className="wrap" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <img src={img}></img>
+           <p> Page not found </p>
+            <Link to={'/'}>Back to Home</Link>
+        </div>
         </div>
     )
 }
